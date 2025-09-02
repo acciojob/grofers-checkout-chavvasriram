f99beb5229f8221bad2871e70e873e334fd71ef6
chavvasriram <chavvasriram@gmail.com>
2025-09-02 10:55:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const  allprices=document.querySelectorAll(".price")
+		const table=document.getElementById("table")
+		function getSumandAppend(){
+			let totalsum=0
+			for(let price of allprices){
+				totalprice+=parseInt(price.innerText)
+			}
+			const newrow=document.createElement("tr")
+			const newdata=document.createElement("td")
+			newdata.colSpan=2
+			newdata.innerText=`Total price: ${totalsum}`
+			newrow.appendchild(newdata)
+			table.appendchild(newrow)
+		}
 };
 
 getSumBtn.addEventListener("click", getSum);
